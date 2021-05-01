463b0f2107f30bc811534d608efb0671bf2a890b
ancobacr <ancobacr@gmail.com>
2021-05-01 05:58:02 +0000

    map created

--- src/models.py
+++ src/models.py
@@ -8,23 +8,38 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True) 
+    firstName = Column(String(250))
+    lastName = Column(String(250))
+    email = Column(String(250)) 
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    age = Column(Integer)
+    description = Column(String(250))
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    population = Column(Integer)
+    capital = Column(String(250))
+    description = Column(String(250))
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+
+class Favorite(Base):  
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User) 
+    character = relationship('Characters', back_populates="favorite") 
+    planet = relationship('Planets', back_populates="favorite")
+   
 
     def to_dict(self):
         return {}
